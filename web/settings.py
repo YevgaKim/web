fe7f6a1dc138f7e3ec1c7334a6a574b688a78c1b
YevgaKim <jekanew09@gmail.com>
2023-07-23 18:14:39 +0300

    doesn't matter

--- web/settings.py
+++ web/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 
 STATIC_URL ='static/'
 MEDIA_ROOT = "/var/www/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
